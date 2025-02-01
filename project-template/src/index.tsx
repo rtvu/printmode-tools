370c071489e8709f2e256d98afff46a86c42bf8e
Ryan Vu <ryanvu87@gmail.com>
2025-02-01 17:43:18 +0000

    Cleaned up template components

---
+++ project-template/src/index.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const rootElement = document.getElementById("root");
+if (rootElement !== null) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      Hello Word
+    </StrictMode>,
+  );
+}
