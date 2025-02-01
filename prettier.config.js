3fd5821d35468016a9bfc24026d783e1e8aeffe3
Ryan Vu <ryanvu87@gmail.com>
2025-02-01 19:48:58 +0000

    Added `prettier-plugin-organize-imports`

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,7 @@
 const config = {
+  plugins: ["prettier-plugin-organize-imports"],
   printWidth: 120,
+  organizeImportsSkipDestructiveCodeActions: true,
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
-import { loadEnv, defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
