e54ee2cd64257c5de5946b1354ad3e4c6b10c337
Ryan Vu <ryanvu87@gmail.com>
2025-02-09 19:39:52 +0000

    Added shortcut to toggle theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { useEffect } from "react";
 
 export function Navbar() {
   const [isDarkTheme, setIsDarkTheme] = useLocalStorage<boolean>("isDarkTheme", false);
+
+  useEffect(() => {
+    const keydownHandler = (event: KeyboardEvent) => {
+      if (!event.altKey && event.ctrlKey && !event.metaKey && !event.shiftKey && event.key === "i") {
+        setIsDarkTheme(!isDarkTheme);
+      }
+    };
+
+    window.addEventListener("keydown", keydownHandler);
+
+    return () => {
+      window.removeEventListener("keydown", keydownHandler);
+    };
+  }, [isDarkTheme, setIsDarkTheme]);
 
   return (
     <div className="navbar bg-base-content px-8 py-3">
